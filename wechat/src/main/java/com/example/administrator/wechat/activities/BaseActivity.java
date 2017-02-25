b15a41e5451c80f6ce8ad62c005f823b84ced510
Bryant <907508273@qq.com>
2017-02-25 18:48:43 +0800

    Http示例

---
+++ wechat/src/main/java/com/example/administrator/wechat/activities/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.example.administrator.wechat.activities;
+
+import android.app.Activity;
+
+/**
+ * Created by bryant on 2017/2/25.
+ */
+
+public class BaseActivity extends Activity {
+}
---
+++ wechat/src/main/java/com/example/administrator/wechat/tools/Constants.java
@@ -0,0 +1,10 @@
+package com.example.administrator.wechat.tools;
+
+/**
+ * Created by bryant on 2017/2/25.
+ */
+
+public class Constants {
+    public static final String serverUrl = "http://localhost:8080/";
+    public static final String baidu = "http://baidu.com/";
+}
